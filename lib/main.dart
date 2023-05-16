be4e5a3f01494170a930886e16685e406266c4c1
frankjmclarke <frankjmclarke@gmail.com>
2023-05-15 21:23:12 -0700

    Add comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,134 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx/bind.dart';
+import 'package:getx/models/user.dart';
+//import 'package:get/get_navigation/src/routes/default_transitions.dart';
+import 'package:getx/utils/translations.dart';
+import 'package:getx/views/first.dart';
+import 'package:getx/views/second.dart';
+import 'package:getx/views/third.dart';
 
-import 'controller.dart';
-
-/*
-Overall, this code demonstrates a basic implementation of GetX for state
-management in Flutter, where GetBuilder is used to update the UI based on
-changes to a Controller class, and the Get package is used for routing.
- */
 void main() {
   runApp(GetMaterialApp(
+    // It is not mandatory to use named routes, but dynamic urls are interesting.
     initialRoute: '/home',
+    defaultTransition: Transition.native,
+    translations: MyTranslations(),
+    locale: const Locale('pt', 'BR'),
     getPages: [
-      GetPage(name: '/home', page: () => Home(), binding: SampleBind()),
+      //Simple GetPage
+      GetPage(name: '/home', page: () => First()),
+      // GetPage with custom transitions and bindings
+      GetPage(
+        name: '/second',
+        page: () => Second(),
+        customTransition: SizeTransitions(),
+        binding: SampleBind(),
+      ),
+      // GetPage with default transitions
+      GetPage(
+        name: '/third',
+        transition: Transition.cupertino,
+        page: () => Third(),
+      ),
     ],
   ));
 }
 
-/*
-The Home class extends StatelessWidget and is the entry point of the app. In
-the build method, a Scaffold widget is returned, which has an app bar, a
-body, and a floating action button.
- */
-class Home extends StatelessWidget {
-  final controller = Get.put(Controller());
-
+class SizeTransitions extends CustomTransition {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Counter")),
-      /*
-The body contains a Center widget with a Column widget inside. The
-Column contains a GetBuilder widget, which is used to build the UI based on
-the state of the Controller class. The builder method receives a callback
-function that is called whenever the state of the Controller changes. In this
-case, the Text widget inside the GetBuilder widget displays the value of the
-count variable of the Controller class.
-       */
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(() => Text(
-                  'clicks: ${controller.count}',
-                )),
-            ElevatedButton(
-              child: const Text('Next Route'),
-              /*
-The onPressed property of the ElevatedButton widget is set to a callback
-function that calls the Get.to method to navigate to the Second route when
-the button is pressed.
-               */
-              onPressed: () {
-                Get.to(Second());
-              },
-            ),
-          ],
+  Widget buildTransition(
+      BuildContext context,
+      Curve? curve,
+      Alignment? alignment,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    return Align(
+      alignment: Alignment.center,
+      child: SizeTransition(
+        sizeFactor: CurvedAnimation(
+          parent: animation,
+          curve: curve!,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => controller.increment(), //callback
-        child: const Icon(Icons.add),
+        child: child,
       ),
     );
   }
 }
-
-/*
-The Second class is another StatelessWidget that displays the value of
-count of the Controller class, which is retrieved using Get.find().
- */
-class Second extends StatelessWidget {
-  final Controller ctrl = Get.find();
-
-  @override
-  Widget build(context) {
-    return Scaffold(body: Center(child: Text("ZZZ ${ctrl.count}")));
+class Controller extends GetxController {
+  int count = 0;
+  void increment() {
+    count++;
+    // use update method to update all count variables
+    update();
   }
 }
-/*
 
- */
+class SampleBind extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ControllerX>(() => ControllerX());
+  }
+}
+
+
+
+class ControllerX extends GetxController {
+  final count1 = 0.obs;
+  final count2 = 0.obs;
+  final list = [56].obs;
+  final user = User().obs;
+
+  updateUser() {
+    user.update((value) {
+      value!.name = 'Jose';
+      value.age = 30;
+    });
+  }
+
+  /// Once the controller has entered memory, onInit will be called.
+  /// It is preferable to use onInit instead of class constructors or initState method.
+  /// Use onInit to trigger initial events like API searches, listeners registration
+  /// or Workers registration.
+  /// Workers are event handlers, they do not modify the final result,
+  /// but it allows you to listen to an event and trigger customized actions.
+  /// Here is an outline of how you can use them:
+
+  /// made this if you need cancel you worker
+  late Worker _ever;
+
+  @override
+  onInit() {
+    super.onInit();
+    /// Called every time the variable $_ is changed
+    _ever = ever(count1, (_) => print("$_ has been changed (ever)"));
+
+    everAll([count1, count2], (_) => print("$_ has been changed (everAll)"));
+
+    /// Called first time the variable $_ is changed
+    once(count1, (_) => print("$_ was changed once (once)"));
+
+    /// Anti DDos - Called every time the user stops typing for 1 second, for example.
+    debounce(count1, (_) => print("debouce$_ (debounce)"),
+        time: const Duration(seconds: 1));
+
+    /// Ignore all changes within 1 second.
+    interval(count1, (_) => print("interval $_ (interval)"),
+        time: const Duration(seconds: 1));
+  }
+
+  int get sum => count1.value + count2.value;
+
+  increment() => count1.value++;
+
+  increment2() => count2.value++;
+
+  disposeWorker() {
+    _ever.dispose();
+    // or _ever();
+  }
+
+  incrementList() => list.add(75);
+}
+
